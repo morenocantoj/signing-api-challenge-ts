32b9cd02da23e08ceb05eff1a634a394376f137d
Jaume Moreno <morenocantoj@gmail.com>
2022-12-19 16:11:44 +0100

    chore: move some crypto config values

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,10 @@ export const config = {
   server: {
     port: process.env.PORT || 3000,
   },
+  crypto: {
+    salt: process.env.CRYPTO_SALT || 'fiskaly is AWESOME!',
+    rsa: {
+      passphrase: process.env.RSA_PASSPHRASE || 'fiskaly is AWESOME',
+    },
+  },
 }
--- src/crypto/ecdsa.ts
+++ src/crypto/ecdsa.ts
@@ -1,14 +1,19 @@
-import { KeyPair } from './generation';
-import crypto from 'crypto';
+import { KeyPair } from './generation'
+import crypto from 'crypto'
+import { config } from '../config'
 
 export default function sign(dataToBeSigned: string, keyPair: KeyPair): string {
   try {
-    const cipher = crypto.createCipheriv('aes-256-gcm', crypto.scryptSync(keyPair.private, 'fiskaly is AWESOME!', 32), crypto.randomBytes(16));
-    let encrypted = cipher.update(dataToBeSigned);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return encrypted.toString('base64');
+    const cipher = crypto.createCipheriv(
+      'aes-256-gcm',
+      crypto.scryptSync(keyPair.private, config.crypto.salt, 32),
+      crypto.randomBytes(16)
+    )
+    let encrypted = cipher.update(dataToBeSigned)
+    encrypted = Buffer.concat([encrypted, cipher.final()])
+    return encrypted.toString('base64')
   } catch (e) {
-    console.log(e);
-    throw e;
+    console.log(e)
+    throw e
   }
 }
