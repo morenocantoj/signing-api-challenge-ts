5d37d59fe0c217bb48bf87fd2e2cd94a8c9bef3e
Jaume Moreno <morenocantoj@gmail.com>
2022-12-19 19:25:03 +0100

    feat: basic Device class

---
+++ src/devices/domain/Device.ts
@@ -0,0 +1,47 @@
+import { generateId } from '../../shared/domain/generateId'
+
+type DeviceAttributes = {
+  id: string
+  signatureAlgorithm: string
+  signaturesPerformed: number
+  label?: string
+}
+
+export class Device {
+  private static MIN_SIGNATURES_PERFORMED = 0
+  private id: string
+  private signatureAlgorithm: string
+  private signaturesPerformed: number
+  private label?: string
+
+  constructor(attributes: DeviceAttributes) {
+    if (attributes.signaturesPerformed < Device.MIN_SIGNATURES_PERFORMED) {
+      throw new Error(`Signatures performed can be less than ${Device.MIN_SIGNATURES_PERFORMED}`)
+    }
+
+    this.id = attributes.id
+    this.signatureAlgorithm = attributes.signatureAlgorithm
+    this.signaturesPerformed = attributes.signaturesPerformed
+    this.label = attributes.label
+  }
+
+  static create(
+    newDeviceAttributes: Pick<DeviceAttributes, 'label' | 'signatureAlgorithm'>
+  ): Device {
+    return new Device({
+      id: generateId(),
+      signatureAlgorithm: newDeviceAttributes.signatureAlgorithm,
+      signaturesPerformed: 0,
+      label: newDeviceAttributes.label,
+    })
+  }
+
+  serialize() {
+    return {
+      id: this.id,
+      signatureAlgorithm: this.signatureAlgorithm,
+      signaturesPerformed: this.signaturesPerformed,
+      label: this.label,
+    }
+  }
+}
